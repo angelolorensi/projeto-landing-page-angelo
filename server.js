91dcf8c1f20176f7f7e32d79534ef6ffd0100b13
angelo lorensi <angelomozza@gmail.com>
2022-11-05 01:00:23 -0300

    deploy no heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 8080;
+const PORT= process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/projeto-landing-page-angelo'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/projeto-landing-page-angelo/index.html');
+  res.sendFile(__dirname + '/dist/projeto-landing-page-angelo/index.html')
 });
 
 app.listen(PORT, () => {
-  console.log('Servidor iniciado na porta ' + PORT);
+  console.log('Servidor iniciado na porta: ' + PORT);
 })
